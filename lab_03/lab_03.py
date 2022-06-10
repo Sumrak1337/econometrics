b581870f03134972f13b02e89fbb521e2fea9af2
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-06-10 10:26:15 +0300

    rewrote lab_03 with class

--- lab_03/lab_03.py
+++ lab_03/lab_03.py
@@ -5,81 +5,89 @@ import scipy.stats as ss
 from data.lab_data import LabData
 
 
-def kolmogorov(edf):
-    left_value = np.max(np.array([i / n - edf.y[i] for i in range(1, n + 1)]))
-    right_value = np.max(np.array([edf.y[i] - (i - 1) / n for i in range(1, n + 1)]))
-    dn = max(left_value, right_value)
-    dn = dn * (np.sqrt(n) - 0.01 + 0.85 / np.sqrt(n))
+class Lab03:
 
-    # report
-    print('Критерий Колмогорова')
+    def __init__(self, main_data: dict):
+        self.data = np.array([])
+        for value in main_data.values():
+            self.data = np.append(self.data, value)
+        self.alphas = [0.05, 0.1]
+        self.mod_quantiles = {0.15: [0.775, 0.091],
+                              0.10: [0.819, 0.104],
+                              0.05: [0.895, 0.126],
+                              0.025: [0.955, 0.148],
+                              0.01: [1.035, 0.178]}
+        self.n = None
+        self.mean = None
+        self.var = None
 
-    for alpha in alphas:
-        answer = False
-        d_star = mod_quantiles[alpha][0]
+    def processing(self):
+        diffs = np.array([self.data[i] - self.data[i - 1] for i in range(1, len(self.data))])
+        diffs = np.unique(diffs.round(decimals=2))
+        self.n = len(diffs)
 
-        if dn <= d_star:
-            answer = True
+        self.mean = np.mean(diffs)
+        self.var = np.var(diffs, ddof=1)
 
-        if answer:
-            print(f'Гипотеза H0 принимается на уровне значимости {alpha}, так как значение {dn:.8} не попадает в интервал ({d_star}; +inf)')
-        else:
-            print(f'Гипотеза H0 отвергается на уровне значимости {alpha}, так как значение {dn:.8} попадает в интервал ({d_star}; +inf)')
-    print()
+        min_diffs = np.min(diffs)
+        max_diffs = np.max(diffs)
+        cum_freq = ss.cumfreq(diffs, numbins=self.n-1, defaultreallimits=(min_diffs, max_diffs))
+        cum_freq = np.append([1], cum_freq[0])
+        freq = cum_freq / self.n
+
+        intervals = np.linspace(min_diffs, max_diffs, self.n)
+        ecdf = sm.distributions.StepFunction(intervals, freq, side='right')
+
+        self.kolmogorov(ecdf)
+        self.omega(ecdf)
+
+    def kolmogorov(self, edf):
+        left_value = np.max(np.array([i / self.n - edf.y[i] for i in range(1, self.n + 1)]))
+        right_value = np.max(np.array([edf.y[i] - (i - 1) / self.n for i in range(1, self.n + 1)]))
+        dn = max(left_value, right_value)
+        dn = dn * (np.sqrt(self.n) - 0.01 + 0.85 / np.sqrt(self.n))
+
+        # report
+        print('Критерий Колмогорова')
+
+        for alpha in self.alphas:
+            answer = False
+            d_star = self.mod_quantiles[alpha][0]
+
+            if dn <= d_star:
+                answer = True
+
+            if answer:
+                print(f'Гипотеза H0 принимается на уровне значимости {alpha}, так как значение {dn:.8} не попадает в интервал ({d_star}; +inf)')
+            else:
+                print(f'Гипотеза H0 отвергается на уровне значимости {alpha}, так как значение {dn:.8} попадает в интервал ({d_star}; +inf)')
+        print()
+
+    def omega(self, edf):
+        s = 0
+        for i in range(1, self.n + 1):
+            s += (edf.y[i] - (2 * i - 1) / (2 * self.n)) ** 2
+        omega_sqr = 1 / (12 * self.n) + s
+
+        omega_sqr = omega_sqr * (1 + 0.5 / self.n)
+
+        # report
+        print('Критерий omega^2')
+
+        for alpha in self.alphas:
+            answer = False
+            omega_star = self.mod_quantiles[alpha][1]
+
+            if omega_sqr <= omega_star:
+                answer = True
+
+            if answer:
+                print(f'Гипотеза H0 принимается на уровне значимости {alpha}, так как значение {omega_sqr:.8} не попадает в интервал ({omega_star}; +inf)')
+            else:
+                print(f'Гипотеза H0 отвергается на уровне значимости {alpha}, так как значение {omega_sqr:.8} попадает в интервал ({omega_star}; +inf)')
 
 
-def omega(edf):
-    s = 0
-    for i in range(1, n + 1):
-        s += (edf.y[i] - (2 * i - 1) / (2 * n)) ** 2
-    omega_sqr = 1 / (12 * n) + s
+if __name__ == '__main__':
+    dict_data = LabData().lab_03()
 
-    omega_sqr = omega_sqr * (1 + 0.5 / n)
-
-    # report
-    print('Критерий omega^2')
-
-    for alpha in alphas:
-        answer = False
-        omega_star = mod_quantiles[alpha][1]
-
-        if omega_sqr <= omega_star:
-            answer = True
-
-        if answer:
-            print(f'Гипотеза H0 принимается на уровне значимости {alpha}, так как значение {omega_sqr:.8} не попадает в интервал ({omega_star}; +inf)')
-        else:
-            print(f'Гипотеза H0 отвергается на уровне значимости {alpha}, так как значение {omega_sqr:.8} попадает в интервал ({omega_star}; +inf)')
-
-
-dict_data = LabData().lab_03()
-
-data = np.array([])
-for value in dict_data.values():
-    data = np.append(data, value)
-
-alphas = [0.05, 0.1]
-mod_quantiles = {0.15: [0.775, 0.091],
-                 0.10: [0.819, 0.104],
-                 0.05: [0.895, 0.126],
-                 0.025: [0.955, 0.148],
-                 0.01: [1.035, 0.178]}
-
-diffs = np.array([data[i] - data[i - 1] for i in range(1, len(data))])
-diffs = np.unique(diffs.round(decimals=2))
-n = len(diffs)
-
-mean = np.mean(diffs)
-var = np.var(diffs, ddof=1)
-
-min_diffs = np.min(diffs)
-max_diffs = np.max(diffs)
-cum_freq = ss.cumfreq(diffs, numbins=n-1, defaultreallimits=(min_diffs, max_diffs))
-cum_freq = np.append([1], cum_freq[0])
-freq = cum_freq / n
-
-intervals = np.linspace(min_diffs, max_diffs, n)
-ecdf = sm.distributions.StepFunction(intervals, freq, side='right')
-
-kolmogorov(ecdf)
-omega(ecdf)
+    Lab03(dict_data).processing()
