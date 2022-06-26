18303e228e3ad6e5db1668d446fbe37a64dac92b
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-06-27 01:13:33 +0300

    lab_08 added report

--- lab_08/lab_08.py
+++ lab_08/lab_08.py
@@ -2,11 +2,21 @@ import numpy as np
 import scipy.stats as ss
 
 from data.lab_data import LabData
+from pathlib import Path
+
+RESULT_ROOT = Path(__file__).parent.resolve() / 'report' / 'figures'
 
 
 class Lab08:
-    def __init__(self, main_data: dict):
+    def __init__(self, main_data: dict, stream):
+        self.stream = stream
         self.samples = [value for value in main_data.values()]
+
+        data = open(RESULT_ROOT / 'data.txt', 'w', encoding='utf-8')
+        data.write('Исходные данные\n')
+        for k, v in main_data.items():
+            data.write(f'{k}: {v}\n')
+        data.close()
 
         self.Q1 = None
         self.Q2 = None
@@ -32,22 +42,24 @@ class Lab08:
         self.Q2 = q2
         self.Q = q
 
+        self.stream.write(f'Q1 = {self.Q1:.3f}\n')
+        self.stream.write(f'Q2 = {self.Q2:.3f}\n')
+        self.stream.write(f'Q = Q1 + Q2 = {self.Q:.3f}\n\n')
         critical_value = ss.f.ppf(1 - self.alpha, dfn=self.k-1, dfd=self.n-self.k)
         intergroup_var = q1 / (self.k - 1)
         intragoup_var = q2 / (self.n - self.k)
         f_value = intergroup_var / intragoup_var
 
         # report
-        print('Критерий Фишера')
+        self.stream.write('Критерий Фишера\n')
         if f_value > critical_value:
-            print(f'Гипотеза H0 о равенстве средних отклоняется, так как F_наблюдаемое > F_критическое ({f_value:.4} > {critical_value:.4})')
-            print()
+            self.stream.write(f'Гипотеза H0 о равенстве средних отклоняется, \nтак как F_наблюдаемое > F_критическое ({f_value:.4} > {critical_value:.4})\n\n')
             self.linear_contrast_method([self.samples[0], self.samples[1]])
             self.linear_contrast_method([self.samples[0], self.samples[2]])
             self.linear_contrast_method([self.samples[1], self.samples[2]])
             self.linear_contrast_method([self.samples[0], self.samples[1], self.samples[2]])
         else:
-            print(f'Гипотеза H0 о равенстве средних принимается, так как F_наблюдаемое <= F_критическое ({f_value:.4} <= {critical_value:.4})')
+            self.stream.write(f'Гипотеза H0 о равенстве средних принимается, \nтак как F_наблюдаемое <= F_критическое ({f_value:.4} <= {critical_value:.4}\n\n)')
 
     def get_global_mean(self):
         len_sum = 0
@@ -81,18 +93,18 @@ class Lab08:
         # report
         if k == 2:
             if left < np.mean(s[0]) - np.mean(s[1]) < right:
-                print(f'Гипотеза H0 о равенстве двух средних принимается, так как ноль содержится в доверительном интервале ({left:.4}; {right:.4})')
+                self.stream.write(f'Гипотеза H0 о равенстве двух средних принимается, \nтак как ноль содержится в доверительном интервале ({left:.4}; {right:.4})\n\n')
             else:
-                print(f'Гипотеза H0 о равенстве двух средних отвергается, так как ноль не содержится в доверительном интервале ({left:.4}; {right:.4})')
+                self.stream.write(f'Гипотеза H0 о равенстве двух средних отвергается, \nтак как ноль не содержится в доверительном интервале ({left:.4}; {right:.4})\n\n')
 
         else:
             m1 = np.mean(s[0])
             m2 = np.mean(s[1])
             m3 = np.mean(s[2])
             if left < 0.5 * (m1 + m3) - m2 < right:
-                print(f'Гипотеза H0 о равенстве средних принимается, так как ноль содержится в доверительном интервале ({left:.4}; {right:.4})')
+                self.stream.write(f'Гипотеза H0 о равенстве средних принимается, \nтак как ноль содержится в доверительном интервале ({left:.4}; {right:.4})\n\n')
             else:
-                print(f'Гипотеза H0 о равенстве средних отвергается, так как ноль не содержится в доверительном интервале ({left:.4}; {right:.4})')
+                self.stream.write(f'Гипотеза H0 о равенстве средних отвергается, \nтак как ноль не содержится в доверительном интервале ({left:.4}; {right:.4})\n\n')
 
     def get_conf_ints(self, elc, ed, k, n):
         f = ss.f.ppf(self.alpha, dfn=k-1, dfd=n-k)
@@ -101,5 +113,7 @@ class Lab08:
         return left, right
 
 
+file = open(RESULT_ROOT / 'file.txt', 'w', encoding='utf-8')
 dict_data = LabData().lab_08_data
-Lab08(dict_data).processing()
+Lab08(dict_data, file).processing()
+file.close()
