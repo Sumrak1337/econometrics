6e3a0d532d06af5064a2cc5037836cabe866e288
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-06-27 08:07:42 +0300

    lab_10 added report

--- lab_10/lab_10.py
+++ lab_10/lab_10.py
@@ -2,17 +2,31 @@ import numpy as np
 import scipy.stats as ss
 
 from data.lab_data import LabData
+from pathlib import Path
+
+RESULT_ROOT = Path(__file__).parent.resolve() / 'report' / 'figures'
+# TODO: rewrite from Sonya
 
 
 class Lab10:
-    def __init__(self, main_data: dict):
+    def __init__(self, main_data: dict, stream):
+        self.stream = stream
         self.data = np.array([value for value in main_data.values()])
+
+        data = open(RESULT_ROOT / 'data.txt', 'w', encoding='utf-8')
+        data.write('Исходные данные\n')
+        for k, v in main_data.items():
+            data.write(f'{k}: {v}\n')
+        data.close()
 
         self.n, self.k = self.data.shape
         self.alpha = 0.05
         self.mean = np.mean(self.data)
         self.row_mean = np.mean(self.data, axis=1)
         self.column_mean = np.mean(self.data, axis=0)
+
+        self.stream.write(f'Средние по столбцам: {self.column_mean.round(3)}\n')
+        self.stream.write(f'Средние по строкам: {self.row_mean.round(3)}\n\n')
 
     def processing(self):
         s1 = self.n * np.sum((self.row_mean - self.mean) ** 2)
@@ -23,9 +37,9 @@ class Lab10:
                 s3 += (self.data[i][j] - self.row_mean[i] - self.column_mean[j] + self.mean) ** 2
 
         # report
-        print('Двухфакторный дисперсионный анализ')
-        print('1ая гипотеза: H0: b_i = 0')
-        print('2ая гипотеза: H0: t_i = 0')
+        self.stream.write('Двухфакторный дисперсионный анализ\n')
+        self.stream.write('1ая гипотеза: H0: b_i = 0\n')
+        self.stream.write('2ая гипотеза: H0: t_i = 0\n\n')
 
         f1 = (s1 / (self.n - 1)) / (s3 / ((self.n - 1) * (self.k - 1)))
         f2 = (s2 / (self.k - 1)) / (s3 / ((self.n - 1) * (self.k - 1)))
@@ -33,15 +47,17 @@ class Lab10:
         f_crit2 = ss.f.ppf(1 - self.alpha, dfn=self.k-1, dfd=(self.n-1)*(self.k-1))
 
         if f1 >= f_crit1:
-            print(f'Вторая нулевая гипотеза H0 отвергается на уровне значимости {self.alpha} при условии принятия первой нулевой гипотезы, так как {f1} >= {f_crit1}')
+            self.stream.write(f'Вторая нулевая гипотеза H0 отвергается \nна уровне значимости {self.alpha} при условии принятия первой нулевой гипотезы, \nтак как {f1:.3f} >= {f_crit1:.3f}\n\n')
         else:
-            print(f'Вторая нулевая гипотеза H0 принимается на уровне значимости {self.alpha} при условии принятия первой нулевой гипотезы, так как {f1} < {f_crit1}')
+            self.stream.write(f'Вторая нулевая гипотеза H0 принимается \nна уровне значимости {self.alpha} при условии принятия первой нулевой гипотезы, \nтак как {f1:.3f} < {f_crit1:.3f}\n\n')
 
         if f2 >= f_crit2:
-            print(f'Первая нулевая гипотеза H0 отвергается на уровне значимости {self.alpha} при условии принятия второй нулевой гипотезы, так как {f2} >= {f_crit2}')
+            self.stream.write(f'Первая нулевая гипотеза H0 отвергается \nна уровне значимости {self.alpha} при условии принятия второй нулевой гипотезы, \nтак как {f2:.3f} >= {f_crit2:.3f}\n\n')
         else:
-            print(f'Первая нулевая гипотеза H0 принимается на уровне значимости {self.alpha} при условии принятия второй нулевой гипотезы, так как {f2} < {f_crit2}')
+            self.stream.write(f'Первая нулевая гипотеза H0 принимается \nна уровне значимости {self.alpha} при условии принятия второй нулевой гипотезы, \nтак как {f2:.3f} < {f_crit2:.3f}\n\n')
 
 
+file = open(RESULT_ROOT / 'file.txt', 'w', encoding='utf-8')
 dict_data = LabData.lab_10()
-Lab10(dict_data).processing()
+Lab10(dict_data, file).processing()
+file.close()
